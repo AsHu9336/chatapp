51d03c53eb7bb733c97a324b83384e373af260db
Ashutosh Kumar Singh <ashutoshkumarsingh3946@gmail.com>
2024-06-04 07:46:02 +0530

    commit

--- chat_app/src/App.js
+++ chat_app/src/App.js
@@ -1,7 +1,9 @@
 import React from "react"
+import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Join from "./Components/Join";
 import Chat from "./Components/Chat";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 
 function App() {
--- chat_app/src/Components/Join.js
+++ chat_app/src/Components/Join.js
@@ -1,10 +1,23 @@
 import React from "react";
 
-const Join = function(){
+const Join = function () {
 
     return (
         <>
-        <h1>Join</h1>
+
+            <div className=" min-vh-100 d-flex flex-column justify-content-center align-items-center bg-warning">
+                <h1 className="text-center m-2">Join</h1>
+                <form role="search">
+                    <input class="form-control m-2" type="search" placeholder="Name" aria-label="Search" />
+                    <input class="form-control m-2" type="search" placeholder="Code" aria-label="Search" />
+                    <div class="text-center">
+                        <button type="button" class="btn btn-outline-danger">SignIn</button>
+                    </div>
+                </form>
+
+
+
+            </div>
         </>
     )
 }
